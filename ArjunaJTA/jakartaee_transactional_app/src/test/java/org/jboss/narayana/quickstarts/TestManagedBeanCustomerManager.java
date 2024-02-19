2be0475df6690dd92130f7f1945ee85393e42da8
Manuel Finelli <jfinelli@redhat.com>
2024-02-19 12:00:20 +0000

    [TEMP] Created quickstart that uses byteman out of the box

--- ArjunaJTA/jakartaee_transactional_app/src/test/java/org/jboss/narayana/quickstarts/TestManagedBeanCustomerManager.java
+++ ArjunaJTA/jakartaee_transactional_app/src/test/java/org/jboss/narayana/quickstarts/TestManagedBeanCustomerManager.java
@@ -6,8 +6,13 @@ import java.util.List;
 
 import jakarta.inject.Inject;
 
+import jakarta.transaction.TransactionManager;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.extension.byteman.api.BMRule;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.narayana.quickstarts.ejb.CustomerManagerEJB;
 import org.jboss.narayana.quickstarts.ejb.CustomerManagerEJBImpl;
 import org.jboss.narayana.quickstarts.jpa.Customer;
@@ -21,13 +26,49 @@ import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.extras.creaper.core.online.FailuresAllowedBlock;
+import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
+import org.wildfly.extras.creaper.core.online.OnlineOptions;
+import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
 import static org.junit.Assert.fail;
 
 @RunWith(Arquillian.class)
+@ServerSetup(value = TestManagedBeanCustomerManager.ServerSetup.class)
 public class TestManagedBeanCustomerManager {
+
+	public static class ServerSetup implements ServerSetupTask {
+		@Override
+		public void setup(ManagementClient managementClient, String containerId) throws Exception {
+			OnlineManagementClient creaper = org.wildfly.extras.creaper.core.ManagementClient.online(
+					OnlineOptions.standalone().wrap(managementClient.getControllerClient()));
+
+			try(FailuresAllowedBlock allowedBlock = creaper.allowFailures()) {
+				creaper.execute("/subsystem=logging/periodic-rotating-file-handler=FILE:write-attribute(name=level,value=TRACE)");
+				creaper.execute("/subsystem=logging/console-handler=CONSOLE:write-attribute(name=level,value=TRACE)");
+				creaper.execute("/subsystem=logging/logger=com.arjuna:write-attribute(name=level,value=TRACE)");
+			}
+			new Administration(creaper).reload();
+		}
+
+		@Override
+		public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
+			OnlineManagementClient creaper = org.wildfly.extras.creaper.core.ManagementClient.online(
+					OnlineOptions.standalone().wrap(managementClient.getControllerClient()));
+
+			try(FailuresAllowedBlock allowedBlock = creaper.allowFailures()) {
+				creaper.execute("/subsystem=logging/periodic-rotating-file-handler=FILE:write-attribute(name=level,value=WARN)");
+				creaper.execute("/subsystem=logging/console-handler=CONSOLE:write-attribute(name=level,value=WARN)");
+				creaper.execute("/subsystem=logging/logger=com.arjuna:write-attribute(name=level,value=WARN)");
+			}
+		}
+	}
+
 	@Inject
 	private CustomerManagerManagedBean managedBeanCustomerManager;
+
+	@Inject
+	TransactionManager transactionManager;
 
 	@Deployment
 	public static WebArchive createDeployment() {
@@ -51,8 +92,14 @@ public class TestManagedBeanCustomerManager {
 	}
 
 	@Test
+	@BMRule(
+			name = "Delays the commitment of the transaction by 5 seconds",
+			targetClass = "com.arjuna.ats.arjuna.coordinator.BasicAction", targetMethod = "prepare(boolean)",
+			targetLocation = "EXIT",
+			action = "debug(\"**** Byteman script checkListCustomers **** -> Delay of 5 seconds!\"), delay(10000)")
 	public void checkListCustomers() throws Exception {
 
+		transactionManager.setTransactionTimeout(1);
 		// Create a customer
 		managedBeanCustomerManager.addCustomer("Test"
 				+ System.currentTimeMillis());
@@ -66,8 +113,14 @@ public class TestManagedBeanCustomerManager {
 		assertTrue(firstList.size() < secondList.size());
 	}
 	@Test
+	@BMRule(
+			name = "Delays the commitment of the transaction by 5 seconds",
+			targetClass = "com.arjuna.ats.arjuna.coordinator.BasicAction", targetMethod = "prepare(boolean)",
+			targetLocation = "EXIT",
+			action = "debug(\"**** Byteman script checkCustomerCount **** -> Delay of 5 seconds!\"), delay(5000)")
 	public void checkCustomerCount() throws Exception {
 		int response = -1;
+		transactionManager.setTransactionTimeout(1);
 		int size = managedBeanCustomerManager.getCustomerCount();
 
 		// Create a new customer
@@ -99,8 +152,14 @@ public class TestManagedBeanCustomerManager {
 		size = response;
 	}
 	@Test
+	@BMRule(
+			name = "Delays the commitment of the transaction by 5 seconds",
+			targetClass = "com.arjuna.ats.arjuna.coordinator.BasicAction", targetMethod = "prepare(boolean)",
+			targetLocation = "EXIT",
+			action = "debug(\"**** Byteman script testCustomerCountInPresenceOfRollback **** -> Delay of 5 seconds!\"), delay(5000)")
 	public void testCustomerCountInPresenceOfRollback() throws Exception {
 		int response = -1;
+		transactionManager.setTransactionTimeout(1);
 		int size = managedBeanCustomerManager.getCustomerCount();
 
 		String firstCustomerName = "Test" + System.currentTimeMillis();
